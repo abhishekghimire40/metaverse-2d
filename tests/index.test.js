820b00b569db396417b770e582586ec3148482b8
abhishekghimire40 <abhishekghimire40@gmail.com>
2024-12-28 18:06:55 -0600

    wrote 4 authentication test for signup and signin

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 function sum(a, b) {
   return a + b;
 }
@@ -6,7 +8,7 @@ const BACKEND_URL = "http://localhost:3000";
 
 describe("Authentication", () => {
   test("User is able to sign up", async () => {
-    const username = "kirat" + Math.random();
+    const username = "abhishek" + Math.random();
     const password = "123456";
 
     const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
@@ -23,5 +25,49 @@ describe("Authentication", () => {
       type: "admin",
     });
     expect(response.statusCode).toBe(400);
+  });
+
+  test("Signup request fails if the username is empty", async () => {
+    const username = `Abhishek-${Math.random()}`;
+    const password = "123456";
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      password,
+    });
+    expect(response.statusCode).toBe(400);
+  });
+
+  test("Signin succeeds if the username and password are correct.", async () => {
+    const username = `Abhishek-${Math.random()}`;
+    const password = "123456";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+    });
+
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.token).toBeDefined();
+  });
+
+  test("Signin fails if the username and password donot match", async () => {
+    const username = `Abhishek-${Math.random()}`;
+    const password = "123456";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      username,
+      password,
+    });
+    const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      username: "WrongUsername",
+      password,
+    });
+
+    expect(response.statusCode).toBe(403);
   });
 });
